6c18bdd6047b06f5ece5d0cda2f0b139d9677346
Ethan Tran <68451114+Hyancinth@users.noreply.github.com>
2023-05-20 16:59:04 -0400

    Update newModel.py
    Fix imports, add comments

--- newModel.py
+++ newModel.py
@@ -1,52 +1,44 @@
-
-
-import os
-import warnings
-import cv2
-import tensorflow.keras as keras
 import matplotlib.pyplot as plt
-import matplotlib.style as style
 import numpy as np
-import pandas as pd
-from PIL import Image
-from tensorflow.keras import models, layers, optimizers
+from tensorflow.keras import models, layers
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.models import Model
-from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.layers import Dropout
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-
+#Load data
 xData = np.load('xData.npy')
 yData = np.load('yData.npy')
 
-filePath =  './models/savedModel.h5'
 
+filePath =  './models/savedModel.h5' # Path to save model
+ 
+#Callbacks
 modelCheckpoint = ModelCheckpoint(filepath = filePath, save_best_only = True)
 earlyStopping = EarlyStopping(monitor = 'val_loss', patience = 10, verbose = 1, mode = 'auto', restore_best_weights = True, min_delta = 0)
 
+#Split data 80:20, train:test
 xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.2, random_state = 42, stratify = yData)
+
 print("xTrain shape:", xTrain.shape)
 print("yTrain shape:", yTrain.shape)
 print("xTest shape:", xTest.shape)
 print("yTest shape:", yTest.shape)
 
-plt.imshow(xTrain[0], cmap='gray')
-plt.show()
 
 print("Sample xTrain data:")
-print(xTrain[0])  # Print the first sample in xTrain
+print(xTrain[0])  
 
 print("Sample yTrain data:")
-print(yTrain[0])  # Print the corresponding label for the first sample in yTrain
+print(yTrain[0])  
 
 print("Sample xTest data:")
-print(xTest[0])  # Print the first sample in xTest
+print(xTest[0])  
 
 print("Sample yTest data:")
-print(yTest[0])  # Print the corresponding label for the first sample in yTest
+print(yTest[0])  
 
-
+#Create model
 model = models.Sequential()
 model.add(layers.Conv2D(32, (5, 5), strides=(2, 2), activation='relu', input_shape=(224, 224,1)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -69,6 +61,7 @@ model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accur
 print("Training Model\n")
 history = model.fit(xTrain, yTrain, epochs=200, batch_size=16, verbose=1, validation_data=(xTest, yTest), callbacks = [modelCheckpoint, earlyStopping])
 
+#Plot accuracy and loss
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
@@ -77,7 +70,6 @@ plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
-# summarize history for loss
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
@@ -90,6 +82,7 @@ print("Evaluating Model\n")
 results = model.evaluate(xTest, yTest, batch_size=128)
 print(results)
 
+#Classification report
 yPred = model.predict(xTest)
 yTrue = np.argmax(yTest, axis = 1)
 report = classification_report(yTrue, np.argmax(yPred, axis = 1))
